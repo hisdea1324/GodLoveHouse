361337933445ef63d9066edd30740ae4ed2c916e
hisdea1324 <hisdea@me.com>
2013-07-27 22:12:27 +0900

    멤버 오브젝트 컨버팅 완료~

--- include/dataFormat/MemberObject.php
+++ include/dataFormat/MemberObject.php
@@ -172,39 +172,34 @@ class MemberObject {
 			$stmt->close();
 			
 		} else {
-			
-			$query = "UPDATE god_room SET ";
-			$updateData = "`houseId` = ?, ";
-			$updateData = $updateData."`roomName` = ?, ";
-			$updateData = $updateData."`limit` = ?, ";
-			$updateData = $updateData."`explain` = ?, ";
-			$updateData = $updateData."`network` = ?, ";
-			$updateData = $updateData."`kitchen` = ?, ";
-			$updateData = $updateData."`laundary` = ?, ";
-			$updateData = $updateData."`fee` = ?, ";
-			$updateData = $updateData."`imageId1` = ?, ";
-			$updateData = $updateData."`imageId2` = ?, ";
-			$updateData = $updateData."`imageId3` = ?, ";
-			$updateData = $updateData."`imageId4` = ? ";
-			$query = $query.$updateData." WHERE `roomId` = ?";
+
+			$query = "UPDATE god_member SET ";
+			$updateData = "`nick` = ?, ";
+			$updateData.= "`userLv` = ?, ";
+			$updateData.= "`email` = ?, ";
+			$updateData.= "`jumin` = ?, ";
+			$updateData.= "`address1` = ?, ";
+			$updateData.= "`address2` = ?, ";
+			$updateData.= "`zipcode` = ?, ";
+			$updateData.= "`phone` = ?, ";
+			$updateData.= "`mobile` = ?, ";
+			$updateData.= "`msgOk` = ? ";
+			$query .= $updateData." WHERE `userId` = ?";
 
 			# create a prepared statement
 			$stmt = $mysqli->prepare($query);
 			
-			$stmt->bind_param("isissssiiiiii", 
-				$this->record['houseId'], 
-				$this->record['roomName'], 
-				$this->record['limit'], 
-				$this->record['explain'], 
-				$this->record['network'], 
-				$this->record['kitchen'], 
-				$this->record['laundary'], 
-				$this->record['fee'], 
-				$this->record['imageId1'], 
-				$this->record['imageId2'], 
-				$this->record['imageId3'], 
-				$this->record['imageId4'], 
-				$this->record['roomId']);
+			$stmt->bind_param("sssssssssi", 
+				$this->record['nick'], 
+				$this->record['userLv'], 
+				$this->record['email'], 
+				$this->record['jumin'], 
+				$this->record['address1'], 
+				$this->record['address2'], 
+				$this->record['zipcode'], 
+				$this->record['phone'], 
+				$this->record['mobile'], 
+				$this->record['msgOk']);
 				
 			# execute query
 			$stmt->execute();
@@ -214,244 +209,23 @@ class MemberObject {
 		}
 	} 
 
+	function Delete() {
+		global $mysqli;
 
-
-
-
-
-
-
-
-
-
-
+		if ($this->record['userId'] > -1) {
+			$stmt = $mysqli->prepare("DELETE FROM god_member WHERE userId = ?");
+			$stmt->bind_param("s", $this->record['userId']);
+			$stmt->execute();
+			$stmt->close();
+		}
+	} 
 
 
 
 }
 
-
-
-
-
-
 /*
 class MemberObject {
-	var $memberRS;
-
-	#  class member variable
-	# ***********************************************
-	var $m_userid;
-	var $m_nick;
-	var $m_name;
-	var $m_userLevel;
-	var $m_email;
-	var $m_jumin;
-	var $m_address1;
-	var $m_address2;
-	var $m_zipcode;
-	var $m_phone;
-	var $m_mobile;
-	var $m_msgOk;
-
-	var $m_registDate;
-	var $m_updateDate;
-	var $m_LastLoginDate;
-
-	var $m_password;
-	var $m_passQuest;
-	var $m_passAnswer;
-
-	#  property getter
-	# ***********************************************
-	function MemberType() {
-
-		switch ($m_userLevel) {
-			case 1:
-				$retValue = "일반회원";
-				break;
-			case 3:
-				$retValue = "선교사";
-				break;
-			case 7:
-				$retValue = "선교관관리자";
-				break;
-			case 9:
-				$retValue = "전체관리자";
-				break;
-		} 
-	} 
-
-	function UserID() {
-		$UserID = $m_userid;
-	} 
-
-	function Nick() {
-		$Nick = $m_nick;
-	} 
-
-	function Name() {
-		$Name = $m_name;
-	} 
-
-	function Level() {
-		$Level = $m_userLevel;
-	} 
-
-	function UserLevel() {
-		$UserLevel = $m_userLevel;
-	} 
-
-	function Email() {
-		$Email = $m_email;
-	} 
-
-	function Jumin() {
-		$Jumin = $m_jumin;
-	} 
-
-	function Address1() {
-		$Address1 = $m_address1;
-	} 
-
-	function Address2() {
-		$Address2 = $m_address2;
-	} 
-
-	function Zipcode() {
-		$Zipcode = $m_zipcode;
-	} 
-
-	function Post() {
-		$Post = $m_zipcode;
-	} 
-
-	function Phone() {
-		$phone = $m_phone;
-	} 
-
-	function Mobile() {
-		$Mobile = $m_mobile;
-	} 
-
-	function CheckMessageOption() {
-		$CheckMessageOption = $m_msgOK;
-	} 
-
-	#  property setter
-	# ***********************************************
-	function UserID($value) {
-		$m_userid = trim($value);
-	} 
-
-	function Nick($value) {
-		$m_nick = trim($value);
-	} 
-
-	function Name($value) {
-		$m_name = trim($value);
-	} 
-
-	function UserLevel($value) {
-		$m_userLevel = trim($value);
-	} 
-
-	function Email($value) {
-		$m_email[0] = trim($value[0]);
-		$m_email[1] = trim($value[1]);
-	} 
-
-	function Jumin($value) {
-		$m_jumin[0] = trim($value[0]);
-		$m_jumin[1] = trim($value[1]);
-	} 
-
-	function Address1($value) {
-		$m_address1 = trim($value);
-	} 
-
-	function Address2($value) {
-		$m_address2 = trim($value);
-	} 
-
-	function Zipcode($value) {
-		$m_zipcode[0] = $value[0];
-		$m_zipcode[1] = $value[1];
-	} 
-
-	function Post($value) {
-		$m_zipcode[0] = trim($value[0]);
-		$m_zipcode[1] = trim($value[1]);
-	} 
-
-	function Phone($value) {
-		$m_phone[0] = trim($value[0]);
-		$m_phone[1] = trim($value[1]);
-		$m_phone[2] = trim($value[2]);
-	} 
-
-	function Mobile($value) {
-		$m_mobile[0] = trim($value[0]);
-		$m_mobile[1] = trim($value[1]);
-		$m_mobile[2] = trim($value[2]);
-	} 
-
-	function CheckMessageOption($value) {
-		if ((strlen(trim($value))==0)) {
-			$m_msgOK=0;
-		} else {
-			$m_msgOK = trim($value);
-		}
-	} 
-
-	function Password($value) {
-		$m_password = trim($value);
-	}
-
-	function PasswordQuestion($value) {
-		$m_passQuest = trim($value);
-	} 
-
-	function PasswordAnswer($value) {
-		$m_passAnswer = trim($value);
-	} 
-
-	#  class initialize
-	# ***********************************************
-	function __construct() {
-		$m_userid="";
-		$m_nick="";
-		$m_name="";
-		$m_userLevel=0;
-		$m_email = array("","");
-		$m_jumin[0]="";
-		$m_jumin[1]="";
-		$m_address1="";
-		$m_address2="";
-		$m_zipcode[0]="";
-		$m_zipcode[1]="";
-		$m_phone = array("","","");
-		$m_mobile = array("","","");
-		$m_msgOk=false;
-		$m_password="";
-		$m_passQuest="";
-		$m_passAnswer="";
-	} 
-
-	function __destruct() {
-		$memberRS = null;
-	} 
-
-	#  class method
-	# ***********************************************
-	function IsEnabledMessage() {
-		return $m_msgOk;
-	} 
-
-	function setUserID($userid) {
-		return Open($userid);
-	}
-
 	function Open($userid) {
 		$query = "SELECT * from users WHERE userId = '".$mssqlEscapeString[$userid]."'";
 		$memberRS = $objDB->execute_query($query);
@@ -555,10 +329,7 @@ class MemberObject {
 		}
 	} 
 
-	function Delete() {
-		$query = "DELETE FROM users WHERE userId = '".$mssqlEscapeString[$m_userId]."'";
-		$objDB->execute_command($query);
-	}
+	
 } 
 */
 ?>
