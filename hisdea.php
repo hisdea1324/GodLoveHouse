bf239e9468194555c0a2ed484a3bd25e28b7f1d8
hisdea1324 <hisdea@me.com>
2013-08-17 03:28:08 +0900

    member object check data

--- hisdea.php
+++ hisdea.php
@@ -1,23 +1,23 @@
 <?php
 	require "include/config.php";
 	require_once($_SERVER['DOCUMENT_ROOT']."/include/dbconn.php");
-	require_once($_SERVER['DOCUMENT_ROOT']."/include/dataFormat/MemberObject.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/include/dataFormat/AccountObject.php");
 
 
 	echo $_SERVER['DOCUMENT_ROOT'];
 
-	$testObject = new MemberObject();
+	$testObject = new AccountObject();
 	//$testObject->Open();
-	$testObject->Update();
+	//$testObject->Update();
 
 
 	//$testObject->Update();
-	print_r($testObject);
+	//print_r($testObject);
 	//$testObject->Delete();
 
 
 	//$testObject->Open(0); 
-	//print_r($testObject);
+	print_r($testObject);
 	//$testObject->Delete(); 
 
 
