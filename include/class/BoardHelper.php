3e5dc41b19a28469948302ad2b9730c12bc00b6c
hisdea1324 <hisdea@me.com>
2013-10-05 13:00:39 +0900

    boardHepler -> record set 으로 변경 boardobject -> session 제거

--- include/class/BoardHelper.php
+++ include/class/BoardHelper.php
@@ -6,22 +6,47 @@
 # last update date : 2009/12/30
 #************************************************************
 class BoardHelper {
+	/*
 	var $m_eHandler;
 
 	var $m_total;
 	var $m_pageCount;
 	var $m_pageUnit;
 	var $m_StrConditionQuery;
+	*/
+	protected $record = array();
 
 	# property
 	#***********************************************
+	
+	
+	public function __set($name,$value) { 
+		$this->record[$name] = $value;
+	}
+	
+	public function __get($name) { 
+		return $this->record[$name];
+	}
+	
+	public function __isset($name) {
+		return isset($this->record[$name]); 
+  }
 
+  function __construct() {
+		$this->record['total'] = 0;
+		$this->record['pageCount'] = 5;
+		$this->record['pageUnit'] = 10;
+		$this->record['strConditionQuery'] = "";
+	}
+	
+	/*
 	function __construct() {
 		$this->m_eHandler = new ErrorHandler();
 		$this->m_pageCount = 5;
 		$this->m_pageUnit = 10;
 		$this->m_total = 0;
 	} 
+	*/
 
 	function __destruct() {
 
@@ -70,23 +95,24 @@ class BoardHelper {
 
 	function makePagingHTML($curPage) {
 		global $mysqli;
-		$query = "SELECT COUNT(*) AS recordCount from board".$this->m_StrConditionQuery;
+		$query = "SELECT COUNT(*) AS recordCount from board".$this->record['strConditionQuery'];
 		if ($result = $mysqli->query($query)) {
 			while($row = $result->fetch_array()) {
 				$this->m_total = $row["recordCount"];
 			}
 		}
 
-		return makePagingN($curPage, $this->m_pageCount, $this->m_pageUnit, $this->m_total);
+		return makePagingN($curPage, $this->record['pageCount'], $this->record['pageUnit'], $this->record['total']);
 	} 
 
 	function getBoardListWithPaging($curPage) {
 		global $mysqli;
 		
-		$topNum = $this->m_pageCount * $curPage;
+		$topNum = $this->record['pageCount'] * $curPage;
 		$board = array();
-
-		$query = "SELECT * FROM board {$this->m_StrConditionQuery} ORDER BY answerId DESC, answerNum DESC LIMIT {$topNum}, {$this->m_pageCount}";
+		
+		$query = "SELECT * FROM board ".$this->record['strConditionQuery']." ORDER BY answerId DESC, answerNum DESC LIMIT {$topNum}, ".$this->record['pageCount'];
+		
 		if ($result = $mysqli->query($query)) {
 			while ($row = $result->fetch_array()) {
 				$board[] = new BoardObject($row["id"]);
