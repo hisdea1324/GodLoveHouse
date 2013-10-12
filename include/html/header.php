53716a8bf14c89a602b70db9d05eed33334fcb36
LIFO-sookbun <sookbun@lifointeractive.com>
2013-10-12 17:36:48 +0900

    메뉴 디자인 수정

--- include/html/header.php
+++ include/html/header.php
@@ -46,46 +46,46 @@
 	<div id="sub_menu0" onpropertychange="selectbox_hidden('sub_menu0');" style="position: absolute; z-index: 999; position: absolute; top:58px; left:200px; display:none;">
 		<table width="451" border="0" cellspacing="0" cellpadding="0" class="submenu">
 			<tr>
-				<td><img src="[WEBROOT]images/common/submenu_l.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_l.gif"></td>
 				<td class="list2"><img src="[WEBROOT]images/common/submenu_0101.gif" onclick="clickTopMenu01(1);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0102.gif" onclick="clickTopMenu01(2);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0103.gif" onclick="clickTopMenu01(3);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0104.gif" onclick="clickTopMenu01(4);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0105.gif" onclick="clickTopMenu01(5);" style="cursor:pointer"></td>
-				<td><img src="[WEBROOT]images/common/submenu_r.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_r.gif"></td>
 			</tr>
 		</table>
 	</div>
 	<div id="sub_menu1" onpropertychange="selectbox_hidden('sub_menu1');" style="position: absolute; z-index: 999; position: absolute; top:58px; left:300px; display:none;">
 		<table width="331" border="0" cellspacing="0" cellpadding="0" class="submenu">
 			<tr>
-				<td><img src="[WEBROOT]images/common/submenu_l.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_l.gif"></td>
 				<td class="list2"><img src="[WEBROOT]images/common/submenu_0202.gif" onclick="clickTopMenu02(2);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0201.gif" onclick="clickTopMenu02(1);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0203.gif" onclick="clickTopMenu02(3);" style="cursor:pointer"></td>
-				<td><img src="[WEBROOT]images/common/submenu_r.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_r.gif"></td>
 			</tr>
 		</table>
 	</div>
 	<div id="sub_menu2" onpropertychange="selectbox_hidden('sub_menu2');" style="position: absolute; z-index: 999; position: absolute; top:58px; left:390px; display:none;">
 		<table width="422" border="0" cellspacing="0" cellpadding="0" class="submenu">
 			<tr>
-				<td><img src="[WEBROOT]images/common/submenu_l.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_l.gif"></td>
 				<td class="list2"><img src="[WEBROOT]images/common/submenu_0401.gif" onclick="clickTopMenu03(4);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0301.gif" onclick="clickTopMenu03(1);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0302.gif" onclick="clickTopMenu03(2);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0303.gif" onclick="clickTopMenu03(3);" style="cursor:pointer"></td>
-				<td><img src="[WEBROOT]images/common/submenu_r.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_r.gif"></td>
 			</tr>
 		</table>
 	</div>
 	<div id="sub_menu3" onpropertychange="selectbox_hidden('sub_menu3');" style="position: absolute; z-index: 999; position: absolute; top:58px; left:730px; display:none;">
 		<table width="165" border="0" cellspacing="0" cellpadding="0" class="submenu">
 			<tr>
-				<td><img src="[WEBROOT]images/common/submenu_l.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_l.gif"></td>
 				<td class="list2"><img src="[WEBROOT]images/common/submenu_0701.gif" onclick="clickTopMenu04(1);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0702.gif" onclick="clickTopMenu04(2);" style="cursor:pointer"></td>
-				<td><img src="[WEBROOT]images/common/submenu_r.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_r.gif"></td>
 			</tr>
 		</table>
 	</div>
@@ -93,35 +93,35 @@
 	<div id="sub_menu4" onpropertychange="selectbox_hidden('sub_menu4');" style="position: absolute; z-index: 999; position: absolute; top:58px; left:600px; display:none;">
 		<table width="245" border="0" cellspacing="0" cellpadding="0" class="submenu">
 			<tr>
-				<td><img src="[WEBROOT]images/common/submenu_l.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_l.gif"></td>
 				<td class="list2"><img src="[WEBROOT]images/common/submenu_0501.gif" onclick="clickTopMenu05(1);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0503.gif" onclick="clickTopMenu05(3);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0502.gif" onclick="clickTopMenu05(2);" style="cursor:pointer"></td>
-				<td><img src="[WEBROOT]images/common/submenu_r.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_r.gif"></td>
 			</tr>
 		</table>
 	</div>
 	<div id="sub_menu5" onpropertychange="selectbox_hidden('sub_menu5');" style="position: absolute; z-index: 999; position: absolute; top:58px; left:580px; display:none;">
 		<table width="384" border="0" cellspacing="0" cellpadding="0" class="submenu">
 			<tr>
-				<td><img src="[WEBROOT]images/common/submenu_l.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_l.gif"></td>
 				<td class="list2"><img src="[WEBROOT]images/common/submenu_0601.gif" onclick="clickTopMenu06(1);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0602.gif" onclick="clickTopMenu06(2);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0604.gif" onclick="clickTopMenu06(3);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0605.gif" onclick="clickTopMenu06(4);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0603.gif" onclick="clickTopMenu06(5);" style="cursor:pointer"></td>
-				<td><img src="[WEBROOT]images/common/submenu_r.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_r.gif"></td>
 			</tr>
 		</table>
 	</div>
 	<div id="sub_menu6" onpropertychange="selectbox_hidden('sub_menu6');" style="position: absolute; z-index: 999; position: absolute; top:58px; left:340px; display:none;">
 		<table width="308" border="0" cellspacing="0" cellpadding="0" class="submenu">
 			<tr>
-				<td><img src="[WEBROOT]images/common/submenu_l.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_l.gif"></td>
 				<td class="list2"><img src="[WEBROOT]images/common/submenu_0901.gif" onclick="clickTopMenu07(1);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0902.gif" onclick="clickTopMenu07(2);" style="cursor:pointer"></td>
 				<td class="list"><img src="[WEBROOT]images/common/submenu_0903.gif" onclick="clickTopMenu07(3);" style="cursor:pointer"></td>
-				<td><img src="[WEBROOT]images/common/submenu_r.gif"></td>
+				<td class="list3"><img src="[WEBROOT]images/common/submenu_r.gif"></td>
 			</tr>
 		</table>
 	</div>
