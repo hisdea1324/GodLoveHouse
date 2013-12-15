51628e97a8aa701ecb408cd5c91af51f02a23799
sookbun <blackdew7@gmail.com>
2013-12-15 22:07:21 +0900

    공유하기

--- community/view.php
+++ community/view.php
@@ -94,7 +94,7 @@ function body() {
 			<!--script src="/include/js/addthis_widget.js" type="text/javascript"></script-->
 			<script language="javascript">
 			function sns_share(sns_type, c_title) {
-				var now_u = "http://"+"<?=$_SERVER['HTTP_HOST']?>"+"<?=$_SERVER['REQUEST_URI']?>";
+				var now_u = "<?=urlencode("http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}")?>";
 				switch(sns_type) {
 					case "fb" : // 페이스북
 						share_url = "http://www.facebook.com/sharer.php?u="+now_u;
