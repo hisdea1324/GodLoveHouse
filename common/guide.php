97bbcc6d07be9895fedb746790412a652aca0d1f
sookbun <blackdew7@gmail.com>
2014-03-22 11:54:00 +0900

    guide popup

---
+++ common/guide.php
@@ -0,0 +1,143 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ko" lang="ko">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>::: God's LoveHouse :::</title>
+<link href="/include/css/style.css" rel="stylesheet" type="text/css">
+<script language='javascript' src='/include/js/flash.js'></script>
+<script language='javascript' src='/include/js/function.js'></script>
+</head>
+<body>
+<div id="popup">
+<div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">홈페이지 이용에 대한 안내를 드립니다.&nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">1. 이용자격</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp; - 파송증명서 혹은 이에 준하는 서류를 제출할 수 있는 분</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">2. 이용방법</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp; 1) 회원가입 후 원하는 선교관을 검색하여, 일정과 체류자에 대한 정보를 입력하시면 됩니다.</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp;&nbsp;&nbsp;&nbsp;(신청에 대한 결과는 '마이페이지 =&gt; 내예약정보' 에서 확인 할 수 있습니다.&nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">﻿</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;"><span id="husky_bookmark_start_1395222983301" style="font-size: 11pt;">﻿</span>&nbsp; 2) 선교관 담당자가 내용을 확인 후 승인 혹은 거절(3일 이내)을 할 수 있습니다.&nbsp;<span id="husky_bookmark_end_1395222983301" style="font-size: 11pt;">﻿</span></span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp; 3) 승인시 선교관 입주 방법과 자세한 내용은 이메일로 안내드립니다.&nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp; 4) 예약금을 갓러브하우스 계좌로 입금함으로 예약이 확정됩니다.</span><br></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;">&nbsp;</div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">#1. 만약 공실이 없는 경우에는 홈페이지 메인화면에 있는 '선교관 이용신청서' 를 다운받아</span><span style="font-size: 11pt;">&nbsp;</span><a href="mailto:godlovehouse@nate.com"><span style="font-size: 11pt;">godlovehouse@nate.com</span></a><span style="font-size: 11pt;">&nbsp;으로 보내주시면 기타 선교관을 통한 일정안내를 도와드리겠습니다.&nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">#2. 선교관을 저희가 직접 소개해 드리지 않은 경우에 '갓러브하우스 소개' 라고 하시면 안됩니다.</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">#3. 정확한 정보를 입력하여 주시기를 부탁드립니다. 선교사님 신분에 대한 확인절차를 각 단체를 통하여 확인하고 있습니다. &nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">&nbsp;</span></div><div style="color: rgb(0, 0, 0); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; orphans: 2; widows: 2; -webkit-text-stroke-width: 0px; -webkit-text-size-adjust: auto;"><span style="font-size: 11pt;">#4. 기타 문의 사항이나 불편사항은</span><span style="font-size: 11pt;">&nbsp;</span><a href="mailto:godlovehouse@nate.com"><span style="font-size: 11pt;">godlovehouse@nate.com</span></a><span style="font-size: 11pt;">&nbsp;으로 보내주시면 감사하겠습니다.</span></div></div>
+</div>
+<!--div id="popup">
+  <p class="popup_tit"><img src="../images/popup/tit_center.gif"></p>
+  <table width="100%" border="0" cellpadding="0" cellspacing="0" class="board_write">
+    <tr>
+      <td class="td01">선교사명</td>
+      <td>
+        <input type="text" name="textfield26" style="width:150px">
+      </td>
+    </tr>
+    <col width="20%" />
+    <col />
+    <tr>
+      <td class="td01">선교지</td>
+      <td>
+        <input type="text" name="textfield262" style="width:150px">
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">국내연락처</td>
+      <td>
+        <input type="text" name="textfield2" style="width:50px">
+        -
+        <input type="text" name="textfield24" style="width:50px">
+        -
+        <input type="text" name="textfield25" style="width:50px">
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">파송기관</td>
+      <td>
+        <input type="text" name="textfield2622" style="width:150px">
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">E-mail</td>
+      <td>
+        <input type="text" name="textfield2632" style="width:100px">
+        @
+        <input type="text" name="textfield2632" style="width:100px">
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">파송일자</td>
+      <td>
+        <input type="text" name="textfield263" style="width:100px">
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">입국일자</td>
+      <td>
+        <input type="text" name="textfield263" style="width:100px">
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">출국예정일</td>
+      <td>
+        <input type="text" name="textfield263" style="width:100px">
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">입국목적</td>
+      <td>
+        <ul>
+          <li>
+            <input name="Input" type="checkbox" value="" class="chk">
+            안식년</li>
+          <li>
+            <input name="Input" type="checkbox" value="" class="chk">
+            질병치료</li>
+          <li>
+            <input name="Input" type="checkbox" value="" class="chk">
+            선교대회</li>
+          <li>
+            <input name="Input" type="checkbox" value="" class="chk">
+            기타</li>
+        </ul>
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">원하시는 서비스 예약 </td>
+      <td>
+        <p>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>쉼터</span>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>핸드폰</span>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>차량제공</span>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>선교우편물 발송업무</span></p>
+        <p>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>소프트웨어 염가 및 무료제공</span>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>콘도 및 휴양지 제공</span></p>
+        <p>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>컴퓨터교육지원</span>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>선교사 재교육 및 사모와 자녀교육</span></p>
+        <p>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>일거리 제공</span>
+          <input name="Input" type="checkbox" value="" class="chk">
+          <span>비상약품 제공서비스</span></p>
+        </ul>
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">추가요청 서비스메뉴 </td>
+      <td>
+        <label>
+        <select name="select">
+          <option>추가요청 메뉴</option>
+        </select>
+        </label>
+      </td>
+    </tr>
+    <tr>
+      <td class="td01">남기고 싶은말</td>
+      <td>
+        <textarea name="textarea"></textarea>
+      </td>
+    </tr>
+  </table>
+  <div class="btn_right"><a href="#"><img src="../images/board/btn_ok.gif" border="0"></a></div>
+</div-->
+<p class="close"><a href="#" onclick="self.close();"><img src="../images/popup/btn_close.gif" border="0"></a></p>
+</body>
+</html>
